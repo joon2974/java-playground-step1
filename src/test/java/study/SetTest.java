ab37f905f851b7647b4006ca4d01256610037c88
joon2974 <vnfmsdyd4@naver.com>
2021-06-06 19:47:53 +0900

    base test, parameterizedTest practive

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드의 기능을 테스트한다.")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contain_2(String input, String expected) {
+        int realInput = Integer.parseInt(input);
+        boolean realExpected = Boolean.parseBoolean(expected);
+
+        assertThat(numbers.contains(realInput)).isEqualTo(realExpected);
+    }
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,43 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split_1() {
+        String values [] = "1,2".split(",");
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    void split_2() {
+        String[] value = "1".split(",");
+        assertThat(value).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String sub = "(1,2)".substring(1, 4);
+        assertThat(sub).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 메소드와 그 예외에 대한 테스트 학습")
+    @Test
+    void charAt() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = "abc".charAt(5);
+                })
+                .withMessageContaining("range: ");
     }
 }
